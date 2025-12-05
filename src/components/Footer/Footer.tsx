6f5590792d4e8632b4386bc2fdf5f6a5d6850229
hishy001-307 <hishii-yusuke455@g.ecc.u-tokyo.ac.jp>
2025-12-05 16:21:53 +0900

    footer brush-up

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,39 +9,25 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className="mx-auto max-w-5xl px-4 py-6">
         <div className={styles.row}>
-          {/* 左側：ロゴ + サイト名 */}
+
+          {/* ブランドロゴ部分 */}
           <div className={styles.brand}>
-            <div className={styles.logoWrap}>
-              <Image
-                src="/cat_nobi.png"
-                alt="Physics Lab. ロゴの猫"
-                width={42}
-                height={42}
-                className={styles.cat}
-              />
-            </div>
-            <div className={styles.brandText}>
-              <span className={styles.siteName}>Physics Lab. 2026</span>
-              <span className={styles.caption}>
-                Department of Physics, The University of Tokyo
-              </span>
-            </div>
+            <Image
+              src="/logo_monochrome.png"  // ← ヘッダーと同じモノクロ版を使用
+              alt="Physics Lab ロゴ"
+              width={5165}
+              height={1989}
+              className={styles.logo}
+            />
+            {/* <span className={styles.siteName}>Physics Lab. 2026</span> */}
           </div>
 
-          {/* 右側：フッターナビ */}
+          {/* ナビ */}
           <nav className={styles.nav}>
-            <Link href="/advent" className={styles.link}>
-              アドベントカレンダー
-            </Link>
-            <Link href="/festival" className={styles.link}>
-              五月祭ポスター
-            </Link>
-            <Link href="/talks" className={styles.link}>
-              学生公演
-            </Link>
-            <Link href="/articles" className={styles.link}>
-              解説記事
-            </Link>
+            <Link href="/advent" className={styles.link}>アドベント</Link>
+            <Link href="/festival" className={styles.link}>五月祭</Link>
+            <Link href="/talks" className={styles.link}>学生講演</Link>
+            <Link href="/articles" className={styles.link}>記事一覧</Link>
           </nav>
         </div>
 
